e85724d14b8e0d735fe59e8817fce0559af021b9
zhangqiang <syrahcava@gmail.com>
2018-04-02 16:11:21 +0800

    update app.js

--- app.js
+++ app.js
@@ -55,6 +55,8 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundsRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function() {
+var port = process.env.PORT || 3000;
+var ip = process.env.IP || "127.0.0.1";
+app.listen(port, ip, function() {
   console.log("Yelpcamp Server start!");
 });
