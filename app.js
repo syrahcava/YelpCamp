812236f700fd0fa35d587a91f55bdff74d5614eb
zhangqiang <syrahcava@gmail.com>
2018-04-03 10:11:01 +0800

    根据express cli调整部分结构

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var bodyParser = require("body-parser");
+var createError = require('http-errors');
+var cookieParser = require('cookie-parser');
 var express = require("express");
 var flash = require("connect-flash");
-var mongoose = require("mongoose");
 var methodOverride = require("method-override");
+var mongoose = require("mongoose");
+var logger = require('morgan');
+var path = require('path');
 var passport = require("passport");
 var localStrategy = require("passport-local");
 
@@ -16,13 +20,22 @@ var indexRoutes = require("./routes/index");
 
 var app = express();
 
-app.set("view engine", "ejs");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+// app.use(express.urlencoded({
+//   extended: false
+// }));
 app.use(methodOverride("_method"));
 app.use(flash());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cookieParser());
 
 app.locals.moment = require("moment");
 
@@ -55,7 +68,20 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundsRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Yelpcamp Server start!");
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
